f7328499ab403ba5a9ae779834a7ef1bc889910c
David Carboni <david.carboni@ros.gov.uk>
2017-07-06 16:56:13 +0100

    Changed from a decorator implementation to a before_request function.

--- src/check.py
+++ src/check.py
@@ -1,52 +1,40 @@
-import os
 import logging
-from flask import request, make_response, redirect
+
+import os
 import requests
+from flask import request, redirect
+
+from src import Keys, Token
 
 log = logging.getLogger(__name__)
 
-SIGN_IN_URL = os.getenv("SIGN_IN_URL", "/sign-in/")
+SIGN_IN_URL = os.getenv("SIGN_IN_URL", "/sign-in")
 log.info("Sign in URL is " + SIGN_IN_URL)
 
 
-def request_jwt(session_id):
+def authorized():
+    # Preference order is header, cookie, session ID
+    token = _get_header_token() or _get_cookie_token()
+    log.debug("Token is " + str(token))
+    if not Token.verify(token, Keys.list_public_keys()):
+        response = redirect(SIGN_IN_URL)
+        response.set_cookie("service", "securities")
+        return response
+
+
+def _get_header_token():
+    authorization = request.headers.get('Authorization')
+    if authorization:
+        parts = authorization.split(" ")
+        if len(parts) == 2 and parts[0] == "Bearer":
+            log.debug("Token from header is: " + parts[1])
+            return parts[1]
+
+
+def _get_cookie_token():
+    session_id = request.cookies.get('jwt-session')
     if session_id:
         response = requests.get("http://auth:5000/token/" + session_id)
         if response.status_code == 200 and response.json():
             log.debug("Retrieved JWT for session " + session_id)
             return response.json().get("token")
-
-
-def validate_token(jwt):
-    # TODO Validate token
-    return True
-
-
-def authorized(f):
-    def wrapper():
-        log.info(request.headers)
-
-        # Preference order is header, cookie, session ID
-        bearer = request.headers.get('Bearer')
-        cookie = request.cookies.get('Bearer')
-        session = request.cookies.get('Bearer-session')
-        if bearer:
-            log.debug("Got JWT from Bearer header")
-        elif cookie:
-            log.debug("Got JWT from cookie")
-        elif session:
-            log.debug("Got session ID from cookie")
-
-        # Get the token, if available
-        jwt = bearer or cookie or request_jwt(session)
-
-        if jwt and validate_token(jwt):
-            log.info("Token is: " + jwt)
-            return f()
-        else:
-            log.info("No token identified. Redirecting to " + SIGN_IN_URL)
-            response = redirect(SIGN_IN_URL)
-            response.set_cookie('service', 'securities')
-            return response
-
-    return wrapper
