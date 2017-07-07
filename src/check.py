2130d896513093cc1e376aaf76965666e6d29a99
David Carboni <david.carboni@ros.gov.uk>
2017-07-07 12:19:44 +0100

    Made the token url configurable.

--- src/check.py
+++ src/check.py
@@ -9,6 +9,7 @@ from src import Keys, Token
 log = logging.getLogger(__name__)
 
 SIGN_IN_URL = os.getenv("SIGN_IN_URL", "/sign-in")
+TOKEN_URL = os.getenv("TOKEN_URL", "http://auth:5000/token")
 log.info("Sign in URL is " + SIGN_IN_URL)
 
 
@@ -34,7 +35,10 @@ def _get_header_token():
 def _get_cookie_token():
     session_id = request.cookies.get('jwt-session')
     if session_id:
-        response = requests.get("http://auth:5000/token/" + session_id)
+        url = TOKEN_URL.strip("/") + "/" + session_id
+        log.debug("Getting token from: " + url)
+        response = requests.get(url)
         if response.status_code == 200 and response.json():
             log.debug("Retrieved JWT for session " + session_id)
             return response.json().get("token")
+        #TODO: catch any errors
